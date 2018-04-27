2ce1fb1629a29f2a665bb34476f4a790fc9eac27
Erica Briga <erica.briga@gmail.com>
2018-04-27 10:24:18 +0100

    account domain

---
+++ src/main/java/com/qa/persistence/domain/Account.java
@@ -0,0 +1,64 @@
+package com.qa.persistence.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Account {
+	
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Id
+	private Long id;
+	@Size(min = 2, max = 100)
+	private String firstName;
+	@Size(min = 2, max = 100)
+	private String secondName;
+	@Size(min = 5, max = 5)
+	private String accountNumber;
+
+	public Account() {
+
+	}
+
+	public Account(String firstName, String secondName, String accountNumber) {
+		this.firstName = firstName;
+		this.secondName = secondName;
+		this.accountNumber = accountNumber;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getSecondName() {
+		return secondName;
+	}
+
+	public void setSecondName(String secondName) {
+		this.secondName = secondName;
+	}
+
+	public String getAccountNumber() {
+		return accountNumber;
+	}
+
+	public void setAccountNumber(String accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+
+}
